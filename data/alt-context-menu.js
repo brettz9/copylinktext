1e84b5f73af0e60b036c89b83f3814e8dc1353c3
Brett Zamir <brettz9@yahoo.com>
2018-03-11 08:08:00 +0800

    Some further, minor work toward webext

--- data/alt-context-menu.js
+++ data/alt-context-menu.js
@@ -2,6 +2,15 @@
 (() => {
 'use strict';
 
+/*
+// Probably won't need this script at all if this is implemented: https://bugzilla.mozilla.org/show_bug.cgi?id=1325814
+const port = browser.runtime.connect({name: 'copylinktext-content-script'});
+port.onMessage.addListener(function () {
+
+});
+port.postMessage();
+*/
+
 self.on('context', function (el) {
     return !!el.alt;
 });
