02fb477f28615a2bfb5b7edb325e88bbfc6c596e
Brett Zamir <brettz9@yahoo.com>
2019-04-20 09:18:55 +0800

    - Add license - Simplify copy - Streamline web-ext-config.js

--- web-ext-config.js
+++ web-ext-config.js
@@ -3,14 +3,12 @@
 module.exports = {
     verbose: true,
     ignoreFiles: [
-        '.DS_Store',
-        '.eslintignore',
-        '.eslintrc',
-        '.remarkrc',
-        '*.md',
-        '*.zip',
-        '*.xpi',
+        // Per https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/web-ext_command_reference#--ignore-files_-i
+        // 1. Any file ending in .xpi or .zip is ignored
+        // 2. Any hidden file (one that starts with a dot) is ignored
+        // 3. Any directory named node_modules is ignored
         'package.json',
+        '*.md',
         'copy-polyfill.js',
         'ignore/**',
         'web-ext-artifacts/**'
